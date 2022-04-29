2fa66320da7dff48112d90da9d130b4c1c758bb1
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-04-30 00:43:10 +0530

    Create Faq Section

---
+++ src/Components/FaqSection.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+  <div>
+      <div className="faq">
+        <h2>
+            Any Questions? <span> FAQ </span>
+        </h2>
+        <div className="Questions">
+           <h4>How Do I Started?</h4>
+           <div className="answer">
+               <p>lorem Lorem ipsom magi noodle panjabi</p>
+               <p>
+               Lorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabi.
+               </p>
+           </div>
+        </div>
+
+        <div className="Questions">
+           <h4>How Do I Started?</h4>
+           <div className="answer">
+               <p>lorem Lorem ipsom magi noodle panjabi</p>
+               <p>
+               Lorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabi.
+               </p>
+           </div>
+        </div>
+
+        <div className="Questions">
+           <h4>How Do I Started?</h4>
+           <div className="answer">
+               <p>lorem Lorem ipsom magi noodle panjabi</p>
+               <p>
+               Lorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabi.
+               </p>
+           </div>
+        </div>
+
+        <div className="Questions">
+           <h4>How Do I Started?</h4>
+           <div className="answer">
+               <p>lorem Lorem ipsom magi noodle panjabi</p>
+               <p>
+               Lorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabiLorem ipsom magi noodle panjabi.
+               </p>
+           </div>
+        </div>
+      </div>
+  </div>
+    );
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutSection from '../Components/AboutSection';
+import FaqSection from '../Components/FaqSection';
 import ServicesSection from '../Components/ServicesSection';
 
 const AboutUs = () => {
@@ -7,6 +8,7 @@ const AboutUs = () => {
       <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
       </>
   );
 }
