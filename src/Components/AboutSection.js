d8fe51ed2664fe624f1a83ac609c3476de0ce5ce
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-06-22 17:06:07 +0530

    responsive design

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -28,18 +28,18 @@ const AboutSection = () => {
        <Description>
            <motion.div className='title'>
                <div className="hide">
-                 <span className="text-2xl "> Hi,There 👋 I'M</span>
-                   <motion.h2 className="text-5xl md:text-7xl"> Rupen Bhimani,</motion.h2>
+                 <span className="text-3xl "> Hi,There 👋 I'M</span>
+                   <motion.h2 className="text-7xl md:text-7xl"> Rupen Bhimani,</motion.h2>
                </div>
                <div className="hide">
-                   <motion.h2 className="text-3xl md:text-7xl md:mt-0 mt-6" variants={titleAnim} initial="hidden" animate="show">Work To Make Your <span>Dreams</span> Come True
+                   <motion.h2 className="text-4xl md:text-7xl md:mt-0 mt-6" variants={titleAnim} initial="hidden" animate="show">Work To Make Your <span>Dreams</span> Come True
                    </motion.h2>
                </div>
                {/* <div className="hide">
                    <h2>True</h2>
                </div> */}
            </motion.div>
-           <motion.p className="text-2xl md:text-3xl">I’m a Web Developer. Working with my hands to make magic happen on the internet. </motion.p>
+           <motion.p className="text-3xl md:text-3xl">I’m a Web Developer. Working with my hands to make magic happen on the internet. </motion.p>
            {/* <motion.button as={Link} to="/work" variants={Button} initial="hidden" animate="show">
             Contact Me
             </motion.button> */}
